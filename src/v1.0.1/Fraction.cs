bbe8c3e113fbfae8d529b13f56cc46aa262317c6
xXmacandcheeseXx <94151338+xXmacandcheeseXx@users.noreply.github.com>
2022-02-02 17:38:16 -0500

    src

---
+++ src/v1.0.1/Fraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fractions
+{
+    public sealed class Fraction
+    {
+        public int top;
+        public int bottom;
+
+        public Fraction(int top, int bottom)
+        {
+            this.top = top;
+
+            if (bottom < 0)
+            {
+                throw new DenominatorCannotLessThanOne();
+            }
+
+            this.bottom = bottom;
+        }
+    }
+
+    class DenominatorCannotLessThanOne : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Denominator must be greater than or equal to one";
+            }
+        }
+    }
+}
