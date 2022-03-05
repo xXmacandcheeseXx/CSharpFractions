6fb9d2b79677a3b255ea1a43c67e0925149f5774
xXmacandcheeseXx <94151338+xXmacandcheeseXx@users.noreply.github.com>
2022-03-05 18:02:39 -0500

    src

---
+++ src/v1.0.2/Fraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fractions
+{
+    public sealed class Fraction
+    {
+        public int top;
+        public int bottom;
+
+        public Fraction(int top, int bottom)
+        {
+            if (bottom < 1)
+            {
+                throw new ArgumentNullException("Denominator must be greater than or equal to one");
+            }
+
+            this.top = top;
+            this.bottom = bottom;
+        }
+    }
+}
